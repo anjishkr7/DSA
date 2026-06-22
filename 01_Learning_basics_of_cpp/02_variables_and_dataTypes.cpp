e391be0b946ed20511aab1cd36bb35dd2fb0d1bf
anjishkr7 <anjish7004254194@gmail.com>
2026-06-22 13:38:16 +0530

    added variables and data types

---
+++ 01_Learning_basics_of_cpp/02_variables_and_dataTypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  //variable defination:-
+  int age=21;//dataType tells us the type of data and its size
+  cout<<"my age is: "<<age<<endl;
+
+  //decleration:-
+  int marks;//give garbage value
+
+  //updation or manipulation:-
+  marks=100;
+  return 0;
+}
