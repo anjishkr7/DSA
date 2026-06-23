c60a856d6cd7cebc92445c2e6cd2618e9638b1c8
anjishkr7 <anjish7004254194@gmail.com>
2026-06-23 14:20:12 +0530

    updated tarnary operator

--- 01_Learning_basics_of_cpp/05_ternaryOperator.cpp
+++ 01_Learning_basics_of_cpp/05_ternaryOperator.cpp
@@ -1,9 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int age;
   cout<<"Enter ur age: ";
+  int age;
   cin>>age;
-  (age>18) ? cout<< "Can drive": cout<<"Cannot Drive";
+  string result=age>18 ? "Can drive": "Cannot Drive";
+  cout<<result<<endl;
   return 0;
 }
