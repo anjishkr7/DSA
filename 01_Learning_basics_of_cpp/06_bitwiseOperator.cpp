863d388877b1ff865d8e9881d602a29ae21f769e
anjishkr7 <anjish7004254194@gmail.com>
2026-06-23 15:10:03 +0530

    Added Bitwise operators

---
+++ 01_Learning_basics_of_cpp/06_bitwiseOperator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+  //bitwise And(&):-
+  cout<<(5&4)<<endl;
+
+  //bitwise OR(|):-
+  cout<<(5|4)<<endl;
+
+  //bitwise NOT(~):-
+  cout<<(~1)<<endl;//number ke sare bits ko flip kr deta hai
+
+  //Bitwise Left shift operator:-
+  cout<<(5<<2)<<endl;//(num*2^k)
+
+  //Bitwise Right shif:-
+  cout<<(5>>2)<<endl;//(num/2^k)
+
+  //Bitwise XOR(^):-same cheez->0 diff 1
+  cout<<(5^5)<<endl;
+
+
+  return 0;
+}
