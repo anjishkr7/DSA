d91e90f4b816c770223f254100098da995bf7d3f
anjishkr7 <anjish7004254194@gmail.com>
2026-06-24 14:58:10 +0530

    added binary to decimal coversion

---
+++ 01_Learning_basics_of_cpp/08_BinaryToDecimal.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int binaryToDecimal(int n){
+  int decimal=0;
+  int i=0;
+  while(n){
+    int bit =n%10;
+    decimal=decimal+bit*pow(2,i++);
+    n/=10;
+  }
+  return decimal;
+
+}
+int main()
+{
+  int binary;
+  cout<<"Enter ur num : ";
+  cin>>binary;
+  int decimal=binaryToDecimal(binary);
+  cout<<decimal;
+  return 0;
+}
