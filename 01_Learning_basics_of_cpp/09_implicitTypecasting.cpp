71df0446cebec7cbc39daa32017fd87abe8f0127
anjishkr7 <anjish7004254194@gmail.com>
2026-06-26 15:08:21 +0530

    Added implicit conversion form int to float

---
+++ 01_Learning_basics_of_cpp/09_implicitTypecasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+//Implicit TypeCasting:-
+int main()
+{
+//1. int to float
+  int num1=6;
+  float num2 =4.5;
+  float sum=num1+num2;
+  cout<<sum;
+  return 0;
+}
