57b424304c4a89985cd3d81f59a0a25f9e9129ab
anjishkr7 <anjish7004254194@gmail.com>
2026-06-27 15:33:50 +0530

    added char to int

--- 01_Learning_basics_of_cpp/09_implicitTypecasting.cpp
+++ 01_Learning_basics_of_cpp/09_implicitTypecasting.cpp
@@ -5,9 +5,14 @@ using namespace std;
 int main()
 {
 //1. int to float
-  int num1=6;
-  float num2 =4.5;
-  float sum=num1+num2;
-  cout<<sum;
+  // int num1=6;
+  // float num2 =4.5;
+  // float sum=num1+num2;
+  // cout<<sum;
+
+//2.char to int
+  // char ch='A';
+  // int a = ch+1;//implicit coversion from char to int
+  // cout<<a<<endl;
   return 0;
 }
