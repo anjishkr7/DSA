24577bf42a9f7f9445d74119e5e4a264b0f909cc
anjishkr7 <anjish7004254194@gmail.com>
2026-08-14 10:29:35 +0530

    added more concepts of data Types

---
+++ 01_Learning_basics_of_cpp/13_dataTypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int age;//variable decalaration
+  string name="Anjish";//variable initialisation
+  char ch='a';
+  float f=11.5;
+  double d=12.1234323432;
+  cout<<sizeof(ch)<<endl;
+  cout<<sizeof(f)<<endl;
+  cout<<sizeof(d)<<endl;
+  return 0;
+}
